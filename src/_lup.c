e5791aafad6afd306993d82a863fd314f83d26e4
yusuke_abe <y.abe@bottlecube.co.jp>
2018-09-29 03:32:36 +0900

    adding Python3 support
    * change PyString_FromString to PyUnicode_FromString
    * add initialize function for Python3

--- src/_lup.c
+++ src/_lup.c
@@ -1,6 +1,21 @@
 #include <Python.h>
 #include "lup.h"
 
+#define MODULE_NAME "lup"
+
+struct module_state {
+   PyObject *error;
+};
+
+#if PY_MAJOR_VERSION >= 3
+#define GET_PYTHON_STRING(X) PyUnicode_FromString(X)
+#define GET_STATE(X)        ((struct module_state *)PyModule_GetState(X))
+#else
+#define GET_PYTHON_STRING(X) PyUnicode_FromString(X)
+#define GET_STATE(X)        (&_state)
+static struct module_state _state;
+#endif
+
 /*
  *  Python API
  */
@@ -35,7 +50,7 @@ static PyObject *_lup_process_chunk(PyObject *self, PyObject *args) {
 
    output = process_chunk(L, input);
    if (output) {
-      py_output = PyString_FromString(output);
+      py_output = GET_PYTHON_STRING(output);
       free(output);
    }
    else {
@@ -46,14 +61,43 @@ static PyObject *_lup_process_chunk(PyObject *self, PyObject *args) {
    return py_output;
 }
 
-
 static PyMethodDef lupMethods[] = {
    {"process_chunk", _lup_process_chunk, METH_VARARGS, "Process this Lua string"},
    {"LuaState", _lup_LuaState, METH_VARARGS, "Create a new Lua State"},
    {NULL, NULL, 0, NULL}
 };
 
+#if PY_MAJOR_VERSION >= 3
+
+static struct PyModuleDef module_def = {
+   PyModuleDef_HEAD_INIT,
+   MODULE_NAME,
+   NULL,
+   sizeof(struct module_state),
+   lupMethods,
+   NULL,
+   NULL,
+   NULL,
+   NULL
+};
+
+PyMODINIT_FUNC PyInit_lup(void) {
+   return PyModule_Create(&module_def);
+}
+
+#else
 
 DL_EXPORT(void) initlup() {
-   Py_InitModule("lup", lupMethods);
+   PyObject *module = Py_InitModule(MODULE_NAME, lupMethods);
+   if(module == NULL) {
+      return;
+   }
+
+   struct module_state *st = GET_STATE(module);
+   st->error = PyErr_NewException(MODULE_NAME ".Error", NULL, NULL);
+   if(st->error == NULL) {
+      Py_DECREF(module);
+   }
 }
+
+#endif
